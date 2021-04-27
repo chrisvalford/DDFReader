9d1d71b3c2e46546a6fdf403d59a2e47e393548c
Christopher Alford <chrisvalford@me.com>
2021-04-27 09:25:50 +0200

    Initial Commit

---
+++ DDFReader/DDFReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  DDFReaderApp.swift
+//  DDFReader
+//
+//  Created by Christopher Alford on 27/4/21.
+//
+
+import SwiftUI
+
+@main
+struct DDFReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
